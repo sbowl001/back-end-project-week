e54dd501d605fd95fd63a767cae02a6a1987df02
Stephanie Bowles <sbowl001@fiu.edu>
2018-07-17 12:29:11 +0800

    setting up mlab

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 
+mongoose.connect('mongodb://sbowl001:database1@ds139841.mlab.com:39841/backendproject')
+.then(()=> {
+    console.log('connected to mlab')
+})
+.catch((error)=> {
+   console.log(error)
+});
 express()
   .get('/newpath', (req, res) => res.status(600).json([1 , 2, 3]))
   .get('/', (req, res) => res.json('pages/index'))
