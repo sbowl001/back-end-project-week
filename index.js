388b40c5cca63b61f80793356a64dad89083b034
Stephanie Bowles <sbowl001@fiu.edu>
2018-07-17 11:52:36 +0800

    test

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 express()
+  .get('/newpath', (req, res) => res.status(600).json([1 , 2, 3]))
   .get('/', (req, res) => res.json('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
